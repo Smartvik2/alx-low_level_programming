5ae32a61e90d89e1ad7b1e60c09b4da530899abc
Mmesoma <excellentmmesoma6@gmail.com>
2022-10-13 13:03:12 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * main - print a text
+ *
+ * return always 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0)
+}

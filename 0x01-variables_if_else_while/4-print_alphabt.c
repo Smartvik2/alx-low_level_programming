4465c3badd999fdeb979224dbbc2a97b70b76baf
Mmesoma <excellentmmesoma6@gmail.com>
2022-10-17 05:34:49 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a' i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == 'e' || i == 'q')
 			continue;
-		putchar(i)
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);

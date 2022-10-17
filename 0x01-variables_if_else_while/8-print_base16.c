fcb513f99652205c66e16dca8086cd38b8d34632
Mmesoma <excellentmmesoma6@gmail.com>
2022-10-17 06:05:15 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main-program entry point.
+ *
+ * return:0 if no error, non zero if error.
+ *
+ **/
+
+int main(void)
+{
+	int i;
+
+	char hexvalues[] =
+		"012345679abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
